3ed7751ee9acd10fbd984690590b309acc150b66
Tac Shadow <67679169+venom7aries@users.noreply.github.com>
2021-12-02 13:11:17 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,112 @@
+//This is just the code for blacklisting the links, discord, client, token and the rest ARENT defined.
+
+//Feel free to edit the link bodies and add new ones to your code as well. Im open for accepting new feature request, just DM me.
+
+//*      contact        *\\
+//Made by The Shadow Himself
+//Discord: Tac Shadow#5920
+//Server:  https://dsc.gg/tacticalshadow
+
+//remove the above lines as well as this one
+
+
+
+client.on('message', message => {
+  if ((message.content.includes("https://dlscord.org/nitro-gift")) || (message.content.includes("http://dlscord.org/nitro-gift"))) {
+    message.delete();
+    message.channel.send("a blacklisted link was deleted!")
+  }
+  
+  if ((message.content.includes("https://lovebird.guru")) || (message.content.includes("http://lovebird.guru"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!")
+  }
+  
+if ((message.content.includes("https://trulove.guru")) || (message.content.includes("http://trulove.guru"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://dateing.club")) || (message.content.includes("http://dateing.club"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://shrekis.life")) || (message.content.includes("http://shrekis.life"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  
+if ((message.content.includes("https://headshot.monster")) || (message.content.includes("http://headshot.monster"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://gamihg-at-my.best")) || (message.content.includes("http://gaming-at-my.best"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://progaming.monster")) || (message.content.includes("http://programing.monster"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://yourmy.monster")) || (message.content.includes("http://yourmg.monster"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://imageshare.best")) || (message.content.includes("http://imageshere.best"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.startsWith("https://screenshot.best")) || (message.content.includes("http://screenshot.best"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+if ((message.content.includes("https://gamingfun.me")) || (message.content.includes("http://gamingfun.me"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://catsnthing.com")) || (message.content.includes("http://catsnthing.com"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.includes("https://catsnthings.fun")) || (message.content.includes("http://catsnthings.fun"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.includes("https://joinmy.site")) || (message.content.includes("http://joinmy.site"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.includes("https://fortnitechat.site")) || (message.content.includes("http://fortnitechat.site"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.includes("https://fortnight.space")) || (message.content.includes("http://fortnight.space"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+if ((message.content.includes("https://freegiftcards.co")) || (message.content.includes("http://freegiftcards.co"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+
+  if ((message.content.includes("https://grabify.link")) || (message.content.includes("http://grabify.link"))) {
+    message.delete();
+    message.channel.send("a blacklisted link of grabify domain was deleted!");
+  }
+  
+});
+
+// if you want to ban the person who sent tne link, just add message.author.ban();
