8ae394472b65a34e0cb30cb29fc324910a50abf2
Tac Shadow <67679169+venom7aries@users.noreply.github.com>
2022-01-26 17:01:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,124 +12,134 @@
 
 
 client.on('message', message => {
+  
+  
+  const grabberEmbed = new Discord.MessageEmbed()
+	grabberEmbed.setTitle("**IP Grabber Detected!**")
+	grabberEmbed.addField("**Username :** ", `${message.author.username}`)
+	grabberEmbed.addField("**User ID :** ", `${message.author.id}`)
+	grabberEmbed.addField("**Domain :** Grabify", `**Link sent :** ||${message}||`)
+	grabberEmbed.setTimestamp()
+  
+  
   if ((message.content.includes("https://dlscord.org/nitro-gift")) || (message.content.includes("http://dlscord.org/nitro-gift"))) {
     message.delete();
-    message.channel.send("a blacklisted link was deleted!")
+    message.channel.send(grabberEmbed)
   }
   
   if ((message.content.includes("https://lovebird.guru")) || (message.content.includes("http://lovebird.guru"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!")
+    message.channel.send(grabberEmbed)
   }
   
 if ((message.content.includes("https://trulove.guru")) || (message.content.includes("http://trulove.guru"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   if ((message.content.includes("https://dateing.club")) || (message.content.includes("http://dateing.club"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   if ((message.content.includes("https://shrekis.life")) || (message.content.includes("http://shrekis.life"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   
 if ((message.content.includes("https://headshot.monster")) || (message.content.includes("http://headshot.monster"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
   if ((message.content.includes("https://gamihg-at-my.best")) || (message.content.includes("http://gaming-at-my.best"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
   if ((message.content.includes("https://progaming.monster")) || (message.content.includes("http://programing.monster"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   if ((message.content.includes("https://yourmy.monster")) || (message.content.includes("http://yourmg.monster"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   if ((message.content.includes("https://imageshare.best")) || (message.content.includes("http://imageshere.best"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
 if ((message.content.startsWith("https://screenshot.best")) || (message.content.includes("http://screenshot.best"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 if ((message.content.includes("https://gamingfun.me")) || (message.content.includes("http://gamingfun.me"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
   if ((message.content.includes("https://catsnthing.com")) || (message.content.includes("http://catsnthing.com"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
 if ((message.content.includes("https://catsnthings.fun")) || (message.content.includes("http://catsnthings.fun"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
 if ((message.content.includes("https://joinmy.site")) || (message.content.includes("http://joinmy.site"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
 if ((message.content.includes("https://fortnitechat.site")) || (message.content.includes("http://fortnitechat.site"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
 
 if ((message.content.includes("https://fortnight.space")) || (message.content.includes("http://fortnight.space"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
 if ((message.content.includes("https://freegiftcards.co")) || (message.content.includes("http://freegiftcards.co"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabbedEmbed);
   }
 
   if ((message.content.includes("https://grabify.link")) || (message.content.includes("http://grabify.link"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
  if ((message.content.includes("https://partpicker.shop")) || (message.content.includes("http://partpicker.shop"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
  if ((message.content.includes("https://websafe.online")) || (message.content.includes("http://websafe.online"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
  if ((message.content.includes("https://sportshub.bar")) || (message.content.includes("http://sportshub.bar"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
  if ((message.content.includes("https://herald.sbs")) || (message.content.includes("http://herald.sbs"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
  if ((message.content.includes("https://locations.quest")) || (message.content.includes("http://locations.quest"))) {
     message.delete();
-    message.channel.send("a blacklisted link of grabify domain was deleted!");
+    message.channel.send(grabberEmbed);
   }
   
 });
